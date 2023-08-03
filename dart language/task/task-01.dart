61688bd56859191efdc57ffa7752f252570c5deb
syedasaminahussain552@gmail.com <syedasaminahussain552@gmail.com>
2023-08-03 19:47:28 +0600

    task  01

---
+++ dart language/task/task-01.dart
@@ -0,0 +1,16 @@
+void main() {
+  const int a = 7, b = 3;
+
+  int add = a + b;
+  int sub = a - b;
+  int multi = a * b;
+  double division = a / b;
+  int mod = a % b;
+
+  print('a = $a , b = $b');
+  print('Addition: $a + $b = $add ');
+  print('Subtruction: $a - $b = $sub ');
+  print('Multiplication: $a * $b = $multi');
+  print('Division : $a / $b = $division ');
+  print('Modulus : $a % $b = $mod');
+}
